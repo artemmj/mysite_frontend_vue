4490596121eb88ca3bc311c690c88b7ecd4bb2b3
Artem_M <web.ep4@gmail.com>
2025-05-26 15:57:07 +0300

    Обновление

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,15 +13,15 @@ import ArticleList from '../components/ArticleList.vue';
 // Моковые данные (можно заменить на запрос к API)
 const articles = ref([
 {
-    id: 13,
+    id: 15,
     filename: "go_grpc.html",
     title: "Go. Статья. gRPC приложение",
-    date: "18 Мая 2025",
+    date: "25 Мая 2025",
     tags: "Go, gRPC",
-    short_content: "WORK IN PROGRESS... Описание и генерация контракта, конфигурация, gRPC-сервер, сервисный слой Auth, Storage, Gracefull Shutdown",
+    short_content: "Описание и генерация контракта, конфигурация, gRPC-сервер, сервисный слой Auth, Storage, Gracefull Shutdown, функциональные тесты (TODO: интеграция)",
 },
 {
-    id: 6,
+    id: 14,
     filename: "go_intro.html",
     title: "Язык программирования Go (Донован, Керниган). Конспект. Вступление",
     date: "23 Мая 2025",
@@ -29,7 +29,7 @@ const articles = ref([
     short_content: "Вступление, Hello World, аргументы командной строки, примеры программ, задания, некоторые мелочи",
 },
 {
-    id: 15,
+    id: 13,
     filename: "go_io.html",
     title: "Go. Потоки и файлы (by metanit.com)",
     date: "19 Мая 2025",
@@ -37,7 +37,7 @@ const articles = ref([
     short_content: "Операции ввода-вывода. Reader и Writer. Работа с  файлами. Стандартные потоки ввода-вывода и io.Copy. Форматированный вывод. Форматируемый ввод. Чтение с консоли. Буферизированный ввод-вывод",
 },
 {
-    id: 14,
+    id: 12,
     filename: "go_async.html",
     title: "Go. Параллельное программирование (by metanit.com)",
     date: "18 Мая 2025",
@@ -45,7 +45,7 @@ const articles = ref([
     short_content: "Горутины. Каналы. Синхронизация. Передача потоков данных. Мьютексы. WaitGroup",
 },
 {
-    id: 12,
+    id: 11,
     filename: "go_rest_api.html",
     title: "Go. Статья. REST API приложение",
     date: "15 Мая 2025",
@@ -53,15 +53,7 @@ const articles = ref([
     short_content: "Конфигурация приложения, логирование, storage, подготовка HTTP сервера, обработчики запросов (handlers), авторизация, тесты (TODO: деплой, больше тестов)",
 },
 {
-    id: 8,
-    filename: "python_base.html",
-    title: "Python. Статья. База (by metanit.com)",
-    date: "19 Апреля 2025",
-    tags: "Python",
-    short_content: "Переменные, арифметические операции, условные конструкции, циклы, фукнции, упаковка и распаковка, лямбды, преобразования типов, декораторы try..except, строки, ООП, списки, кортежи, словари, множества, List Comprehension",
-},
-{
-    id: 11,
+    id: 10,
     filename: "vuejs_forms_comps.html",
     title: "Vue 3. Статья. Формы и компоненты",
     date: "8 Мая 2025",
@@ -69,7 +61,7 @@ const articles = ref([
     short_content: "Двусторонняя привязка данных, формы реактивного добавления, удаления, редактирования, работа с компонентами",
 },
 {
-    id: 10,
+    id: 9,
     filename: "vuejs_base.html",
     title: "Vue 3. Статья. Основы",
     date: "6 Мая 2025",
@@ -77,15 +69,7 @@ const articles = ref([
     short_content: "Структура компонента, data, работа с атрибутами, v-bind, методы, обработчики событий, v-on, data внутри методов, вычисляемые свойства, модификаторы событий, показ по условию, v-if, v-else, v-show, v-for, перебор массивов, стилизация",
 },
 {
-    id: 7,
-    filename: "go_prog_struct.html",
-    title: "Go. Глубже (deprecated)",
-    date: "18 Апреля 2025",
-    tags: "Go",
-    short_content: "Функции (параметры, возврат, тип), указатели, структуры, методы, интерфейсы, полиморфизм",
-},
-{
-    id: 9,
+    id: 8,
     filename: "fastapi_base.html",
     title: "FastAPI. Основы (by metanit.com)",
     date: "28 Апреля 2025",
@@ -93,9 +77,25 @@ const articles = ref([
     short_content: "Обработка запросов и отправка ответов, параметры пути и строки запроса, статус коды, переадресация, статические файлы, даннные запроса, заголовки, куки, формы, SQLAlchemy",
 },
 {
+    id: 7,
+    filename: "python_base.html",
+    title: "Python. Статья. База (by metanit.com) (переработать)",
+    date: "19 Апреля 2025",
+    tags: "Python",
+    short_content: "Переменные, арифметические операции, условные конструкции, циклы, фукнции, упаковка и распаковка, лямбды, преобразования типов, декораторы try..except, строки, ООП, списки, кортежи, словари, множества, List Comprehension",
+},
+{
+    id: 6,
+    filename: "go_prog_struct.html",
+    title: "Go. Глубже (будет удалено)",
+    date: "18 Апреля 2025",
+    tags: "Go",
+    short_content: "Функции (параметры, возврат, тип), указатели, структуры, методы, интерфейсы, полиморфизм",
+},
+{
     id: 5,
     filename: "javascript_funcs.html",
-    title: "JavaScript. Функциональное программирование",
+    title: "JavaScript. Функциональное программирование (удалить/переписать)",
     date: "8 Апреля 2025",
     tags: "JavaScript",
     short_content: "",
@@ -103,7 +103,7 @@ const articles = ref([
 {
     id: 4,
     filename: "javascript_base.html",
-    title: "JavaScript. Основы",
+    title: "JavaScript. Основы (удалить/переписать)",
     date: "7 Апреля 2025",
     tags: "JavaScript",
     short_content: "",
